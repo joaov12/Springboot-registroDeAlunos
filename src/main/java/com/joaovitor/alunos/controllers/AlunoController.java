794b5546d9155d305c641ac46a4f9ba5a98cb2b2
Joao Vitor <jvfs12th@gmail.com>
2023-05-30 17:41:11 -0300

    Controller: @PostMapping, @PutMapping e @DeleteMaping

--- src/main/java/com/joaovitor/alunos/controllers/AlunoController.java
+++ src/main/java/com/joaovitor/alunos/controllers/AlunoController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +21,38 @@ import com.joaovitor.alunos.services.AlunoService;
 @RequestMapping("/alunos")
 public class AlunoController {
 
-   private final AlunoService alunoService;
-
    @Autowired
-   public AlunoController(AlunoService alunoService){
-    this.alunoService = alunoService;
-   }
+   AlunoService alunoService;
 
    @GetMapping("/all")
-   public ResponseEntity<List<Aluno>> getAllAlunos(){
-        return ResponseEntity.status(HttpStatus.OK).body(alunoService.findAllAlunos()); 
+   public ResponseEntity<List<Aluno>> getAllAlunos() {
+      return ResponseEntity.status(HttpStatus.OK).body(alunoService.findAllAlunos());
    }
 
+   @GetMapping("/find/{id}")
+   public ResponseEntity<Aluno> getAlunoById(@PathVariable("id") Long id) {
+      return ResponseEntity.status(HttpStatus.OK).body(alunoService.findAlunoById(id));
+      // Retornar aqui porque nos outros projetos que eu fiz todos fazem esse método
+      // diferente, vamos ver
+      // se assim funciona
+   }
+
+   @PostMapping("/add")
+   public ResponseEntity<Aluno> addAluno(@RequestBody Aluno aluno) {
+      Aluno novoAluno = alunoService.addAluno(aluno);
+      return new ResponseEntity<>(novoAluno, HttpStatus.CREATED);
+   }
+
+   @PutMapping("/edit")
+   public ResponseEntity<Aluno> editAluno(@RequestBody Aluno aluno) {
+      Aluno editarAluno = alunoService.updateAluno(aluno);
+      return new ResponseEntity<>(editarAluno, HttpStatus.OK);
+   }
+
+   @DeleteMapping("/delete/{id}")
+   public ResponseEntity<?> deleteAluno(@PathVariable("id") Long id){
+      alunoService.deleteAluno(id);
+      return new ResponseEntity<>(HttpStatus.OK);
+   }
 
 }
